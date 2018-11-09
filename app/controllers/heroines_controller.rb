c2b5a5892bd790c2d7611b6a93c116193bf6b9fa
github username <github email address>
2018-11-09 11:22:10 -0500

    Done

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,45 @@
 class HeroinesController < ApplicationController
+  before_action :find_heroine, only: [:show, :edit] #:update, :destroy]
+
   def index
     @heroines = Heroine.all
   end
+
+  def show
+    #before_action
+  end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.create(heroine_params)
+    redirect_to @heroine
+  end
+
+  # def edit
+  #   #before_action
+  # end
+
+  # def update
+  #   @heroine.update(heroine_params)
+  #   redirect_to @heroine
+  # end
+
+  # def destroy
+  #   @heroine.destroy
+  #   # redirect_to
+  # end
+
+  private
+
+  def find_heroine
+    @heroine = Heroine.all.find_by(id: params[:id])
+  end
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :description, :power_id)
+  end
+
 end
--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,5 +1,45 @@
 class PowersController < ApplicationController
+  before_action :find_power, only: [:show] #, :edit, :update, :destroy]
+
   def index
     @powers = Power.all
   end
+
+  def show
+    #before_action
+  end
+
+  # def new
+  #   @power = Power.new
+  # end
+  #
+  # def create
+  #   @power = Power.create(power_params)
+  #   redirect_to @power
+  # end
+  #
+  # def edit
+  #   #before_action
+  # end
+  #
+  # def update
+  #   @power.update(power_params)
+  #   redirect_to @power
+  # end
+  #
+  # def destroy
+  #   @power.destroy
+  #   # redirect_to
+  # end
+
+  private
+
+  def find_power
+    @power = Power.all.find_by(id: params[:id])
+  end
+
+  # def power_params
+  #   params.require(:power).permit(:name, :description)
+  # end
+
 end
