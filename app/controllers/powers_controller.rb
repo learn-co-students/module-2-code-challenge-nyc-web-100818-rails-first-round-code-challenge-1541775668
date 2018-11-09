5b9d596c466158dc93e4b842b424dcdb33bcb2a2
ihollander <ianholla@gmail.com>
2018-11-09 10:52:45 -0500

    done

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,5 +1,17 @@
 class PowersController < ApplicationController
+  before_action :find_power, only: :show
+
   def index
     @powers = Power.all
+  end
+  
+  def show
+    # find_power
+  end
+
+  private
+
+  def find_power
+    @power = Power.find_by(id: params[:id])
   end
 end
--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,2 +1,9 @@
 class Heroine < ApplicationRecord
+  belongs_to :power
+
+  validates :super_name, uniqueness: true
+
+  def self.filter_by_power(power)
+    self.joins(:power).where("powers.name like ?", "%#{power}%")
+  end
 end
