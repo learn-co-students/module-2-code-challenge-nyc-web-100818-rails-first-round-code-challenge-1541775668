f06222b6a68a8a4ed8457f91481ddfc72aab8bf6
magic-onion <alexander.m.truong@gmail.com>
2018-11-09 11:24:59 -0500

    done

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -2,4 +2,11 @@ class PowersController < ApplicationController
   def index
     @powers = Power.all
   end
+
+  def show
+    @power = Power.find_by(params[:id])
+    # redirect_to power_path
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :powers, only: [:index]
-  resources :heroines, only: [:index]  
+#deleting the only params was the first thing I did to have access to all the routes while I build
+#then I expanded the actions using 'only:'
+  resources :powers, only: [:index, :show]
+  resources :heroines, only: [:index, :new, :create, :show, :edit, :update]
+  # get '/search', to: 'heroines#search', as: 'heroines_search'
 end
