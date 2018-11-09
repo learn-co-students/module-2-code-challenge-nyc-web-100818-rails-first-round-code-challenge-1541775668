83099ccb5e28e143735c7477a70552466ba4ee1f
Eric Laitman <ericlaitman@Erics-MacBook-Pro-2.local>
2018-11-09 11:20:34 -0500

    final

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,5 +1,26 @@
 class PowersController < ApplicationController
+
+  before_action :find_power, only: [:show]
+
   def index
     @powers = Power.all
   end
+
+  def show
+
+  end
+
+
+
+
+  private
+
+  def find_power
+    @power = Power.find_by(id: params[:id])
+  end
 end
+#
+#   def power_params
+#     params.require(:power).permit(:name, :description)
+#   end
+# end
--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,2 +1,7 @@
 class Heroine < ApplicationRecord
+  belongs_to :power
+  validates :power_id, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true
+
+
 end
