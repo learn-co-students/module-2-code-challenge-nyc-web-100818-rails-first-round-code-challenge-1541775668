b68b3d0e9e8cee6d5de63bfec6297832d840072c
github username <github email address>
2018-11-09 11:23:19 -0500

    Spai Code Challenge

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,51 @@
 class HeroinesController < ApplicationController
+  before_action :find_heroine, only: [:show, :edit, :update]
   def index
     @heroines = Heroine.all
   end
+
+  def show
+    @powers = Power.all
+
+  end
+
+  def new
+    @heroine = Heroine.new
+    @powers = Power.all
+  end
+
+  def create
+    @heroine = Heroine.create(heroine_params)
+    if @heroine.valid?
+      redirect_to @heroine
+    else
+      flash[:errors] = @heroine.errors.full_messages
+      redirect_to new_heroine_path
+    end
+  end
+
+  def edit
+    @powers = Power.all
+  end
+
+  def update
+    @heroine.update(heroine_params)
+    if @heroine.valid?
+      redirect_to @heroine
+    else
+      flash[:errors] = @heroine.errors.full_messages
+      redirect_to edit_heroine_path
+    end
+  end
+
+  private
+
+  def find_heroine
+    @heroine = Heroine.find_by(id: params[:id])
+  end
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
+  end
+
 end
